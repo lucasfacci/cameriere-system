eb8a6c538cc73dfeb27484fb95ff289a48aff3d8
lucasfacci <lucasfacci@protonmail.com>
2023-02-28 23:26:43 -0300

    deleting openfeign core

--- ticket/src/main/java/com/cameriere/ticket/controllers/TicketController.java
+++ ticket/src/main/java/com/cameriere/ticket/controllers/TicketController.java
@@ -1,7 +1,6 @@
 package com.cameriere.ticket.controllers;
 
 import com.cameriere.ticket.services.TicketService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
