781043a0d728c282fb78a720da7a2a1a6f83ccf0
lucasfacci <lucasfacci@protonmail.com>
2024-03-24 17:36:26 -0300

    removing oauth microservice, keycloak will be used instead

--- cameriere-oauth/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_user (id, name, username, email, password) VALUES ('7412b097-3b30-4b69-8fa1-f6e9c4ee011f', 'Nina', 'nina09', 'nina@gmail.com', '$2a$12$kVJthcpPkzOCC8w5dWzPpebaDrRJGcRtniqaQ2fwYetaiT17L8TOC');
-
