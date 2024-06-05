d4e53d821546048b3c000b3dceca52e673a3d820
lucasfacci <lucasfacci@protonmail.com>
2024-06-04 22:48:26 -0300

    fix: some tweaks in gateway configs

--- cameriere-gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
+++ cameriere-gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
@@ -5,8 +5,6 @@ import reactor.core.publisher.Mono;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
@@ -19,11 +17,6 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-	
-	@GetMapping(value = "/token")
-    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
-        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
-    }
 
     @GetMapping("/")
     public Mono<String> index(WebSession session) {
