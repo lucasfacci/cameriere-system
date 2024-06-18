763c4ac2d318b7f2e14a2c70fb60970f2f5853df
lucasfacci <lucasfacci@protonmail.com>
2024-06-18 02:37:34 -0300

    feat: implementing retries in gateway order ms route

--- gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -32,7 +34,10 @@ public class GatewayApplication {
 				.route(p -> p
 						.path("/cameriere/order/**")
 						.filters(f -> f.rewritePath("/cameriere/order/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
 						.uri("lb://ORDER")).build();
 	}
 
