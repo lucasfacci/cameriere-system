3c5caad776e626ed012178b40b8bceff1899e7a6
lucasfacci <lucasfacci@protonmail.com>
2024-06-19 04:43:47 -0300

    feat: rate limiter in gateway routes

--- gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cameriere/gateway/GatewayApplication.java
@@ -3,11 +3,14 @@ package com.cameriere.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -29,7 +32,9 @@ public class GatewayApplication {
 						.filters(f -> f.rewritePath("/cameriere/menu/(?<segment>.*)", "/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.circuitBreaker(config -> config.setName("menuCircuitBreaker")
-										.setFallbackUri("forward:/contactSupport")))
+										.setFallbackUri("forward:/contactSupport"))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://MENU"))
 				.route(p -> p
 						.path("/cameriere/order/**")
@@ -37,8 +42,20 @@ public class GatewayApplication {
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
-										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://ORDER")).build();
 	}
 
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
+	}
 }
